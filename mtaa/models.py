2e3f35acb7f08bc7774d881b4e8eecd25e65b7f0
Tom Chege <emdeechege@gmail.com>
2018-10-17 16:25:17 +0300

    adding models

--- mtaa/models.py
+++ mtaa/models.py
@@ -1,3 +1,43 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+    profile_photo= models.ImageField(upload_to='profiles/',null=True)
+    bio= models.CharField(max_length=240, null=True)
+    email = models.CharField(max_length=100, blank=True, null=True)
+    hood = models.ForeignKey(Hood, on_delete=models.CASCADE, null=True)
+
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+        post_save.connect(create_user_profile, sender=User)
+
+
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def get_profile(cls):
+        profile = Profile.objects.all()
+        return profile
+
+class Hood(models.Model):
+    hood_photo = models.ImageField(upload_to='hood/')
+    hood_name = models.CharField(max_length=100, blank=True, null=True)
+    occupants_count = models.IntegerField(blank=True, null=True)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+
+
+class Business(models.Model):
+    b_photo = models.ImageField(upload_to='business/')
+    b_name = models.CharField(max_length=100, blank=True, null=True)
+    b_description = models.TextField(max_length=200, blank=True, null=True)
+    b_email = models.CharField(max_length=100, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    hood = models.ForeignKey(Hood, on_delete=models.CASCADE, null=True)
+
+    
